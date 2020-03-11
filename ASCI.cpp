5585c70b378a477cc7aac039705531522e743362
kikidark <kikidark@users.noreply.github.com>
2020-03-11 08:32:58 -0600

    Programa de ASCII

---
+++ ASCI.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+
+int main() {
+
+	    std::cout  << (char)205 << (char)205 << (char)205<< (char)205 << (char)205 << (char)205 << (char)205<< (char)205<< (char)205 << (char)205 << (char)205<<"TABLA ASCII"<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205  << "\n";
+	for (int i = 33; i < 128;i++) {
+		
+		std::cout << (char)186 << "\t" << i << ":" << (char)i << "\t" << (char)186;
+		std::cout << (char)186 << "\t" << (i+128) << ":" << (char)(i+128) << "\t" << (char)186<<"\n";
+
+		
+		
+	}
+	    std::cout << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205<<(char)205;
+	return 0;
+}
